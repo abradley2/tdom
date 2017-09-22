80c76d645b1920dbd11117b22770b508f5587841
Tony Bradley <antbradley91@gmail.com>
2017-09-21 21:22:02 -0400

    progress

--- src/patch.js
+++ src/patch.js
@@ -4,6 +4,7 @@ var setAttr = require('./attrs').setAttr
 
 module.exports = function patch (oldVnode, newVnode, render) {
   var parent = oldVnode.dom.parentElement
+  var parentVnode = oldVnode.parentVnode
 
   // if the tag is entirely different,
   // copy over everything fresh and re-render
@@ -11,7 +12,7 @@ module.exports = function patch (oldVnode, newVnode, render) {
     if (oldVnode.root) throw new Error('Cannot change tag of root app element')
 
     var oldChildNode = oldVnode.dom
-    var newChildNode = render(Object.assign(oldVnode, newVnode)).dom
+    var newChildNode = render(null, Object.assign(oldVnode, newVnode), parentVnode).dom
 
     parent.replaceChild(oldChildNode, newChildNode)
 
@@ -28,7 +29,6 @@ module.exports = function patch (oldVnode, newVnode, render) {
 
   // check if any attribtues have changed
   for (var attr in newVnode.attrs) {
-    // TODO: figure out how to handle this
     if (events[attr] && oldVnode.attrs[attr] !== newVnode.attrs[attr]) {
       var handler = newVnode.attrs[attr]
       var wrappedHandler = function () {
@@ -48,14 +48,21 @@ module.exports = function patch (oldVnode, newVnode, render) {
     }
   }
 
-  window.console.log('old = ', oldVnode.children.length, ' new = ', newVnode.children.length)
-  // TODO: not a good way to do this
   var i = 0
   while (i < newVnode.children.length) {
     const child = oldVnode.children[i]
-    if (child && child.attrs) {
-      window.console.log(child.attrs)
+
+    // if the oldVnodes children don't extend this far, time to append!
+    if (!child) {
+      window.console.log(newVnode.children[i])
+      var newEl = render(null, newVnode.children[i], parentVnode)
+      window.console.log(newEl)
+      oldVnode.children[i] = newVnode.children[i]
+      parent.appendChild(newEl)
+    } else {
+    // otherwise we need to figure out how to patch this
     }
+
     patch(oldVnode.children[i], newVnode.children[i], render)
     i = i + 1
   }
